eab20011aa055e2f8fb541369015352d39235dac
13928985360 <532246081@qq.com>
2019-10-13 22:06:15 +0800

    修复bug

--- platform-common/src/main/java/com/platform/utils/RestTemplateConfig.java
+++ platform-common/src/main/java/com/platform/utils/RestTemplateConfig.java
@@ -1,10 +1,22 @@
 package com.platform.utils;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
 
 /**
  * 名称：RestTemplateConfig <br>
@@ -16,17 +28,32 @@ import org.springframework.web.client.RestTemplate;
  */
 @Configuration
 public class RestTemplateConfig {
-
+    Logger logger = LoggerFactory.getLogger(RestTemplateConfig.class);
     @Bean
-    public RestTemplate restTemplate(ClientHttpRequestFactory factory){
-        return new RestTemplate(factory);
+    public RestTemplate restTemplate(){
+        return new RestTemplate(generateHttpsRequestFactory());
     }
 
-    @Bean
-    public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setReadTimeout(5000);//单位为ms
-        factory.setConnectTimeout(5000);//单位为ms
-        return factory;
+
+    public HttpComponentsClientHttpRequestFactory generateHttpsRequestFactory() {
+        try {
+            TrustStrategy acceptingTrustStrategy = (x509Certificates, authType) -> true;
+            SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+            SSLConnectionSocketFactory connectionSocketFactory =
+                    new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
+
+            HttpClientBuilder httpClientBuilder = HttpClients.custom();
+            httpClientBuilder.setSSLSocketFactory(connectionSocketFactory);
+            CloseableHttpClient httpClient = httpClientBuilder.build();
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setHttpClient(httpClient);
+            factory.setConnectTimeout(10 * 1000);
+            factory.setReadTimeout(30 * 1000);
+            return factory;
+        } catch (Exception e) {
+            logger.error("创建HttpsRestTemplate失败", e);
+            throw new RuntimeException("创建HttpsRestTemplate失败", e);
+        }
+
     }
 }
